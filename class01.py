c70929798c3da89e3a6bd171e45060a040e957c7
mosab2021 <id86mosab@gmail.com>
2024-07-28 10:48:52 +0330

    first-commit-28-7

---
+++ class01.py
@@ -0,0 +1,68 @@
+# arian rohani hastam
+# hello
+"""
+a = 10
+b = 15
+c = a + b
+print(c)
+c = a - b
+print(c)
+d =  c * 2
+print(d)
+e = a / 2
+print(e)
+e = a // 2
+print(e)
+e = b % 2
+print(e)
+x = a ** 2
+print(x)
+c  = a > b
+print(c)
+#----------       how are you ? ------
+c  = a < b
+print(c)
+c  = a >= b
+print(c)
+c  = a <= b
+print(c)
+c  = a == b
+print(c)
+c  = a != b
+print(c)
+b = 0
+c = a and b
+print(c)
+c = False or b or 12
+print(c)
+# اين بخش مربوط به اولويت عملگرهاي رياضي نسبت به مقدار دهي  است
+#x = 2
+#x *= 2 + 4
+#print(x)
+
+#y = 12
+#y+= 6 and 0
+#print(y)
+
+a = None
+"""
+
+
+a = 10
+print(type(a))
+
+b = 16.5
+print(type(b))
+
+d = True
+print(type(d))
+
+name = "Arian"
+print(type(name))
+
+e = None
+print(type(e))
+
+
+
+

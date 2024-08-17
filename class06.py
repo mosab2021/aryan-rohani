8d6afdb3973ddd95655584f70868c3badb11d25b
mosab2021 <id86mosab@yahoo.com>
2024-08-17 21:31:15 +0330

    17-8-2024

---
+++ class06.py
@@ -0,0 +1,53 @@
+name = input("please enter your name :")
+family = input("please enter your last name: ")
+print(name)
+print(family)       
+a=input("please enter a number : ")
+b=input("please enter a number : ")
+print(type(a))
+print(type(b))
+a=int(a)
+b=int(b)    
+s= a + b
+print(s)
+        
+    
+name = input("please enter your name :")
+family = input("please enter your last name: ")
+     
+a=input("please enter a number : ")
+b=input("please enter a number : ")
+
+a=int(a)
+b=int(b)
+
+s= a + b
+
+print(s)
+avg=s/2
+print(avg)
+
+a = float(input("please enter a number : "))
+b = int(input("please enter a number : "))
+
+if a > b :
+    print("a بزرگتره",a)
+else:
+    print("b بزگتره",b)
+
+avg = float(input("please enter a number:"))
+if avg==20:
+    print ("best")
+elif avg>=15 and avg<20:
+    print("good")
+elif avg>=10 and avg<15:
+    print("not good")
+elif avg>=0 and avg<10:
+    print("bad")
+else:
+    print("لطفا عدد درست وارد کتيد")
+
+            
+
+    
+      
